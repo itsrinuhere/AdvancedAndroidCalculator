ea77c90cf8b0fa00753961587b0a5643728ffdff
its srinu here <>
2022-04-27 10:28:57 +0530

    updated with app crash.
    solved app crash

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -9,30 +9,36 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditText edittext = (EditText) findViewById(R.id.input);
         Button button = (Button) findViewById(R.id.submit);
-        try{
-            TextView outputview = (TextView) findViewById(R.id.output);
+            TextView outputview = (TextView)findViewById(R.id.output);
             button.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View view) {
-                  int output =0;
-                  if(edittext.getText().toString().matches("(.*)+(.*)")){
-                      String[] data = edittext.getText().toString().split("[+]");
-                      for(String a : data)
-                          output = output+Integer.parseInt(a);
+                  int output = 0;
+                  try {
+                      if (edittext.getText().toString().matches("(.*)+(.*)")) {
+                          String[] data = edittext.getText().toString().split("[+]");
+                          for (String a : data)
+                              output = output + Integer.parseInt(a);
+                               Toast(Integer.toString(output));
+                                outputview.setText(String.valueOf(output));
+                      } else {
+                                outputview.setText(String.valueOf("wrong sequence"));
+                                Toast("wrong sequence");
+                      }
+                  } catch (Exception ex) {
+                         Toast(ex.toString());
                   }
-                  outputview.setText(String.valueOf(output));
-                  }
+              }
           });
-        }catch(Exception ex){
-            Toast.makeText(this,ex.toString(),Toast.LENGTH_LONG).show();
-        }
-    }
+            }//end of the on-create method
 
-}
+    void Toast(String text){
+        Toast.makeText(getBaseContext(),text,Toast.LENGTH_SHORT).show();
+    }
+    }//end of the class
